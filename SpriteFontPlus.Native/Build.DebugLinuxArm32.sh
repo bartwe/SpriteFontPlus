0429233dd84222782bcaa76f3fdf7b00d387f607
Bart van der Werf <bluelive@gmail.com>
2021-12-17 17:22:32 +0000

    fix linux build

--- SpriteFontPlus.Native/Build.DebugLinuxArm32.sh
+++ SpriteFontPlus.Native/Build.DebugLinuxArm32.sh
@@ -4,5 +4,5 @@ mkdir -p bin/DebugLinuxArm32
 mkdir -p obj/DebugLinuxArm32
 rm -f ./bin/DebugLinuxArm32/*.so
 rm -f ./obj/DebugLinuxArm32/*.o
-gcc -c -lm -fPIC -O2 StbTrueType.cpp -o obj/DebugLinuxArm32/LinuxUnifiedBuild.o
+gcc -c -lm -fPIC -O2 LinuxUnifiedBuild.cpp -o obj/DebugLinuxArm32/LinuxUnifiedBuild.o
 gcc -O2 -lm obj/DebugLinuxArm32/LinuxUnifiedBuild.o -o ./bin/DebugLinuxArm32/SpriteFontPlus.Native.so -shared
---
+++ SpriteFontPlus.Native/LinuxUnifiedBuild.cpp
@@ -0,0 +1,3 @@
+#define BW_LINUX
+
+#include "./StbTrueType.cpp"
